03707d5c0dd3273b57ed3db9230f13e752bbb3a7
Subha <sarumugasamysubhasri655@gmail.com>
2025-12-06 09:02:54 +0530

    2commit

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,26 +1,26 @@
 import express from "express";
 import Todo from "../models/Todo.js";
-const express=require("express")
+
 const router = express.Router();
-router.get("/",async(req,res)=>{
-    const todos=await Todo.find({});
+router.get("/", async (req, res) => {
+    const todos = await Todo.find({});
     res.json(todos);
 });
-router.post("/",async(req,res)=>{
-    const newTodo=new Todo({text:req.body.text});
-   const todo=await newTodo.save();
+router.post("/", async (req, res) => {
+    const newTodo = new Todo({ text: req.body.text });
+    const todo = await newTodo.save();
     res.json(todo);
 });
-router.put("/:id",async(req,res)=>{
-    const updateTodo=await Todo.findByIDAndUpdate(
+router.put("/:id", async (req, res) => {
+    const updateTodo = await Todo.findByIdAndUpdate(
         req.params.id,
-        {text:req.body.text,completed:req.body.completed},
-        {new:true}
+        { text: req.body.text, completed: req.body.completed },
+        { new: true }
     );
-    res.json(upadateTodo);
+    res.json(updateTodo);
 });
-router.delete("/:id",async(req,res)=>{
+router.delete("/:id", async (req, res) => {
     await Todo.findByIdAndDelete(req.params.id);
-    res.json({message:"Todo deleted"});
+    res.json({ message: "Todo deleted" });
 });
 export default router;
